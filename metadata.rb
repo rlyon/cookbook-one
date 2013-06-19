13d0f10f3602d00b6c16e350a14f7e2cfb981913
Rob Lyon <nosignsoflifehere@gmail.com>
2013-06-18 17:25:52 -0700

    fixed xmlrpc in sunstone and occi templates

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "rlyon@uidaho.edu"
 license          "All rights reserved"
 description      "Installs/Configures one"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
 
 depends "yum"
 depends "sudo"
